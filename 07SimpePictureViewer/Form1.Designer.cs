0dfbe349a0070f99ee589fe94d98eaf1f6653a84
syado <doi.sinya122@gmail.com>
2018-06-27 19:44:10 +0900

    プロジェクト ファイルを追加します。

---
+++ 07SimpePictureViewer/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace _07SimpePictureViewer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.ファイルToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.開くToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.終了ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.表示ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.フィットToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 24);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(800, 426);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ファイルToolStripMenuItem,
+            this.表示ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // ファイルToolStripMenuItem
+            // 
+            this.ファイルToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.開くToolStripMenuItem,
+            this.終了ToolStripMenuItem});
+            this.ファイルToolStripMenuItem.Name = "ファイルToolStripMenuItem";
+            this.ファイルToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
+            this.ファイルToolStripMenuItem.Text = "ファイル";
+            // 
+            // 開くToolStripMenuItem
+            // 
+            this.開くToolStripMenuItem.Name = "開くToolStripMenuItem";
+            this.開くToolStripMenuItem.Size = new System.Drawing.Size(98, 22);
+            this.開くToolStripMenuItem.Text = "開く";
+            this.開くToolStripMenuItem.Click += new System.EventHandler(this.開くToolStripMenuItem_Click);
+            // 
+            // 終了ToolStripMenuItem
+            // 
+            this.終了ToolStripMenuItem.Name = "終了ToolStripMenuItem";
+            this.終了ToolStripMenuItem.Size = new System.Drawing.Size(98, 22);
+            this.終了ToolStripMenuItem.Text = "終了";
+            // 
+            // 表示ToolStripMenuItem
+            // 
+            this.表示ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.フィットToolStripMenuItem});
+            this.表示ToolStripMenuItem.Name = "表示ToolStripMenuItem";
+            this.表示ToolStripMenuItem.Size = new System.Drawing.Size(43, 20);
+            this.表示ToolStripMenuItem.Text = "表示";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // フィットToolStripMenuItem
+            // 
+            this.フィットToolStripMenuItem.Name = "フィットToolStripMenuItem";
+            this.フィットToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.フィットToolStripMenuItem.Text = "フィット";
+            this.フィットToolStripMenuItem.Click += new System.EventHandler(this.フィットToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.Form1_DragEnter);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem ファイルToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 表示ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 開くToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 終了ToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ToolStripMenuItem フィットToolStripMenuItem;
+    }
+}
+
---
+++ 07SimpePictureViewer/Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _07SimpePictureViewer
+{
+    public partial class Form1 : Form
+    {
+        string[] files = new string[0];
+        string directoryname = "";
+        string filename = "";
+        int index = 0;
+        int MAX = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Openfile(string openFilePass)
+        {
+            int count = 0;
+            if (System.IO.Path.GetExtension(openFilePass) != ".jpg" && System.IO.Path.GetExtension(openFilePass) != ".png")
+            {
+                MessageBox.Show("画像ファイルを選択してください。");
+            }
+            else
+            {
+                System.Drawing.Bitmap bm = new System.Drawing.Bitmap(openFilePass);
+                pictureBox1.Image = bm;
+                directoryname = System.IO.Path.GetDirectoryName(openFilePass);
+                filename = System.IO.Path.GetFileName(openFilePass);
+                System.IO.DirectoryInfo di = new System.IO.DirectoryInfo(directoryname);
+                System.IO.FileInfo[] openfiles = di.GetFiles("*.jpg", System.IO.SearchOption.AllDirectories);
+                foreach (System.IO.FileInfo f in openfiles)
+                {
+                    if (f.FullName == openFilePass)
+                    {
+                        index = count;
+                    }
+                    Array.Resize(ref files, files.Length + 1);
+                    files[count] = f.FullName;
+                    count += 1;
+                }
+            }
+        }
+
+        private void Form1_DragDrop(object sender, DragEventArgs e)
+        {
+            string openFilePass;
+
+            string[] DragDrop_files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            openFilePass = DragDrop_files[0];
+            Openfile(openFilePass);
+        }
+
+        private void Form1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.Copy;
+            }
+        }
+
+        private void 開くToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string openFilePass;
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                openFilePass = openFileDialog1.FileName;
+                Openfile(openFilePass);
+            }
+        }
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            index += 1;
+            if (files.Length > index)
+            {
+                pictureBox1.Image = new System.Drawing.Bitmap(files[index]);
+            }
+            else
+            {
+                index = 0;
+                pictureBox1.Image = new System.Drawing.Bitmap(files[index]);
+            }
+
+        }
+
+        private void フィットToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if(フィットToolStripMenuItem.Checked)
+            {
+                フィットToolStripMenuItem.Checked = false;
+                pictureBox1.SizeMode = PictureBoxSizeMode.AutoSize;
+            }
+            else
+            {
+                フィットToolStripMenuItem.Checked = true;
+                pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            }
+        }
+
+       
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                //矢印キーが押されたことを表示する
+                case Keys.Up:
+                case Keys.Left:
+                    index -= 1;
+                    if (0 < index)
+                    {
+                        pictureBox1.Image = new System.Drawing.Bitmap(files[index]);
+                    }
+                    else
+                    {
+                        index = files.Length - 1;
+                        pictureBox1.Image = new System.Drawing.Bitmap(files[index]);
+                    }
+                    break;
+
+                case Keys.Right:
+                case Keys.Down:
+                    index += 1;
+                    if (files.Length > index)
+                    {
+                        pictureBox1.Image = new System.Drawing.Bitmap(files[index]);
+                    }
+                    else
+                    {
+                        index = 0;
+                        pictureBox1.Image = new System.Drawing.Bitmap(files[index]);
+                    }
+                    break;
+                    
+            }
+        }
+    }
+}
